0e3fb36f4f50ec00b20767502149bc7a96c7b9f3
raulfl1982ndrd <raulfl1982ndrd@gmail.com>
2024-07-29 11:55:28 +0200

    Added horoscope icons, the Horoscope data class and the data provider

---
+++ Horoscopo-IOS/Data/Horoscope.swift
@@ -0,0 +1,16 @@
+//
+//  Horoscope.swift
+//  Horoscopo-IOS
+//
+//  Created by Mañanas on 29/7/24.
+//
+
+import Foundation
+import UIKit
+
+struct Horoscope{
+    let id: String
+    let name: String
+    let logo: UIImage
+    let dates: String
+}
