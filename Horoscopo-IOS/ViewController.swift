3f66719cc0df2c0aba98cdb0f357b076a0d0b710
raulfl1982ndrd <raulfl1982ndrd@gmail.com>
2024-07-29 09:45:13 +0200

    Initial Commit

---
+++ Horoscopo-IOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Horoscopo-IOS
+//
+//  Created by Mañanas on 29/7/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
