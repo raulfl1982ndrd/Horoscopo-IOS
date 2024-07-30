c3ca0608d8ca82bdf30751aaee70679f68688a11
raulfl1982ndrd <raulfl1982ndrd@gmail.com>
2024-07-30 14:06:01 +0200

    Now we can navigate to detail with the selected horoscope as a parameter, we show the horosocope on the detail

---
+++ Horoscopo-IOS/ViewControllers/DetailViewController.swift
@@ -0,0 +1,38 @@
+//
+//  DetailViewController.swift
+//  Horoscopo-IOS
+//
+//  Created by Mañanas on 30/7/24.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+   @IBOutlet weak var logoImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var datesLabel: UILabel!
+    var horoscope:Horoscope? = nil
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        if let horoscope = horoscope{
+            datesLabel.text = horoscope.name
+            logoImageView.image = horoscope.logo
+            datesLabel.text = horoscope.dates
+        }
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Horoscopo-IOS/ViewControllers/ListViewController.swift
+++ Horoscopo-IOS/ViewControllers/ListViewController.swift
@@ -7,13 +7,58 @@
 
 import UIKit
 
-class ListViewController: UIViewController {
+class ListViewController: UIViewController, UITableViewDataSource {
 
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var horoscopeList: [Horoscope] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        horoscopeList = HoroscopeProvider.getAllHoroscopes()
+        tableView.dataSource = self
     }
 
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return horoscopeList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! HoroscopeViewCell
+        
+        let horoscope = horoscopeList[indexPath.row]
+        cell.nameLabel.text = horoscope.name
+        cell.logoImageView.image = horoscope.logo
+        cell.datesLabel.text = horoscope.dates
+        
+        return cell
+    }
+    /*
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+            if indexPath.row == 0 {
+                performSegue(withIdentifier: "Vegetables", sender: self)
+                } else if indexPath.row == 1 {
+                    //another VC
+                } else if indexPath.row == 2 {
+                    //Another VC
+                }
+        }
+    */
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "navigateToDetail"{
+            let viewController = segue.destination as! DetailViewController
+            let indexPath = tableView.indexPathForSelectedRow!
+            viewController.horoscope = horoscopeList[indexPath.row]
+            tableView.deselectRow(at: indexPath, animated: false)
+        }
+    }
+    /*
+    @IBSegueAction func navigateToDetail(_ coder: NSCoder) -> DetailViewController? {
+        return DetailViewController(coder: coder)
+    }
+    */
 }
 
